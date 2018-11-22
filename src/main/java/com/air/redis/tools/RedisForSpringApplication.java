5fc560cea6a6a78d8cbeab029469926cca7169f1
airgilbert <airgilbert@DESKTOP-J78J7MJ>
2018-11-22 10:00:17 +0800

    test update

--- src/main/java/com/air/redis/tools/RedisForSpringApplication.java
+++ src/main/java/com/air/redis/tools/RedisForSpringApplication.java
@@ -1,10 +1,13 @@
 package com.air.redis.tools;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RedisForSpringApplication {
+	public Logger log = LoggerFactory.getLogger(RedisForSpringApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(RedisForSpringApplication.class, args);
